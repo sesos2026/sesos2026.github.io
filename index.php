5ef59581f1d3651482bb740bbc2f117be25c5939
Everton Cavalcante <evertonranielly@gmail.com>
2025-06-20 18:01:56 -0300

    Adding files from the 2025 edition

---
+++ index.php
@@ -0,0 +1,170 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	
+<head>
+	
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style2.css">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link href='http://fonts.googleapis.com/css?family=Average+Sans' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Average' rel='stylesheet' type='text/css'>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+<script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery-1.4.4.min.js"%3E%3C/script%3E'))</script>
+
+
+<title>SESoS 2025</title>
+
+</head>
+
+<body>
+
+<!-- ======================================= SCRIPTS ================================= -->
+
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.2';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+	<script type="text/javascript">
+
+  		var _gaq = _gaq || [];
+  		_gaq.push(['_setAccount', 'UA-49650822-2']);
+  		_gaq.push(['_trackPageview']);
+
+  		(function() {
+   		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  	})();
+	</script>
+	
+<!-- ===================================================================== -->
+
+
+
+<div id="header">
+	<div id="logo">
+		
+		<a href="index.php"><img src="banner-sesos2025.png"  alt="" title="SESoS 2025" /></a><script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.2';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<script async="" src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+<div id="menu">
+		<aside>
+        <nav>
+            <ul id="menu">
+			
+				<li><a href="index.php"> &nbsp HOME</center></a></li> <!--OK-->
+				
+                <li><a href="Organizers.php"> &nbsp WORKSHOP ORGANIZERS</a></li><!-- OK -->
+                
+                <li><a href="Committee.php"> &nbsp PROGRAM COMMITTEE</a></li> <!-- OK -->
+               
+                <li><a href="Dates.php"> &nbsp IMPORTANT DATES</a></li> <!-- OK -->
+                 
+		        <li><a href="Topics.php"> &nbsp TOPICS OF INTEREST</a></li> <!-- OK -->
+
+                <li><a href="Submission.php"> &nbsp PAPER SUBMISSION AND PUBLICATION</a></li>
+
+                <!-- <li><a href="Papers.php"> &nbsp ACCEPTED PAPERS</a></li> -->
+
+                <li><a href="Papers.php"> &nbsp; ACCEPTED PAPERS &nbsp;&nbsp;<font size="10" color="red"><b><i>**NEW**</i></b></font></a></li>
+				
+								<!-- <li><a href="Papers.php"> &nbsp; ACCEPTED PAPERS &nbsp;&nbsp;<font size="10" color="red"><b><i>**NEW**</i></b></font></a></li> -->
+
+				        <!--<li><a href="Publication.php"> &nbsp PUBLICATION OF PAPERS</a></li>  <!-- OK -->
+
+								<!--<li><a href="Keynote.php"> &nbsp; KEYNOTE SPEAKER &nbsp;&nbsp;<font size="10" color="red"><b><i>**NEW**</i></b></font>  </a></li>
+
+                <li><a href="Workshop.php">  &nbsp; WORKSHOP PROGRAM&nbsp;&nbsp;<font size="10" color="red"><b><i>**NEW**</i></b></font>  </a></li>-->
+
+                <!-- <li><a href="Keynote.php"> &nbsp; KEYNOTE SPEAKERS &nbsp;&nbsp;<font size="10" color="red"><b><i>**NEW**</i></b></font>  </a></li> -->
+
+								<li><a href="Keynote.php"> &nbsp; KEYNOTE SPEAKER &nbsp;&nbsp;<font size="10" color="red"><b><i>**NEW**</i></b></font>  </a></li>
+
+                <li><a href="Workshop.php"> &nbsp; WORKSHOP PROGRAM&nbsp;&nbsp;<font size="10" color="red"><b><i>**NEW**</i></b></font>  </a></li>
+
+                <!-- <li><a href="Workshop.php">  &nbsp; WORKSHOP PROGRAM&nbsp;&nbsp;<font size="10" color="red"><b><i>**NEW**</i></b></font>  </a></li> -->
+				
+				<li><a href="Previous.php">  &nbsp PREVIOUS WORKSHOPS</a></li>  <!-- OK -->
+				<li><a href="http://www.sesos-wdes.icmc.usp.br/" target="_BLANK">  &nbsp WORKSHOP SERIES</a></li>  <!-- OK -->
+            </ul>
+			
+        </nav>
+		<a class="twitter-timeline" data-width="250" data-height="500" href="https://twitter.com/sesos2025" target="_blank">Tweets by sesos2025</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+		
+    </aside>
+		
+    <div id="content">
+
+						<!--<div class="fb-page" data-href="https://m.facebook.com/sesos2025/" data-tabs="timeline" data-width="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://m.facebook.com/sesos2025/" class="fb-xfbml-parse-ignore"><a href="https://m.facebook.com/sesos2025/">SESoS/WDES 2019</a></blockquote></div>
+         --> <article>
+
+
+<div class="just2">
+
+<!-- 
+
+<i><h3><font color = "red">*** Date Announcement: ***</h3></font></i>
+
+<p>The organizing team is planning for ICSE 2022 to be held as a hybrid event. Please check information at <u><a href="https://conf.researchr.org/home/icse-2022" target="_blank">ICSE home</a></u>.</p>
+</div>
+
+<br/><br/><br/>
+
+-->
+
+
+
+<!-- <div class="just2">
+
+<i><h3><font color = "red">*** NEWS ***</h3></font></i>
+
+<p>Authors of the best regular papers of SESoS 2024 will be awarded in the workshop closing session and invited to submit extended versions of their works to a Special Issue in the <u><a href="https://www.sciencedirect.com/journal/journal-of-systems-and-software" target="_blank">Journal of Systems and Software</a></u>.</p>
+</div> -->
+
+<!-- <br/><br/> -->
+
+
+<div class="just2">
+
+<h3>Scope</h3>
+
+<p>The 13th International Workshop on Software Engineering for Systems-of-Systems and Software Ecosystems (SESoS 2025) provides researchers and practitioners with a forum to exchange ideas and experiences, analyze research and development issues, discuss promising solutions, and propose theoretical foundations for development and evolution of complex software-intensive systems, inspiring visions for the future of Software Engineering for Systems-of-Systems (SoS) and Software Ecosystems (SECO), as well as paving the way for a more structured community effort.</p>
+
+<p>SoS have become increasingly complex and frequently used in highly distributed, dynamic, and open environments. SoS refer to evolving software systems in which constituent systems (themselves systems in their own right) cooperatively work to fulfill specific, complex missions, facing Software Engineering researchers and practitioners with substantial challenges. SECO have also become an important research topic, addressing social issues along with technical aspects of software development. SoS and SECO are closely related and naturally distributed in development teams. In both fields, distributed resources aim at reducing cost and reaching new Information Technology markets. Software Engineering-related problems and challenges are amplified and become more critical when SoS and SECO are analyzed together.</p>
+
+<p>Following the successful editions of SESoS in 2013-2024, WDES in 2007-2018, and the joint editions of SESoS/WDES in 2017-2021, this year's workshop will focus on coping with challenges and opportunities on the development and evolution of complex software-intensive systems as well as on the modeling and analysis of technological, organizational, and social solutions in this context.</p>
+
+<p>This workshop will be held in conjunction with the <a href="https://conf.researchr.org/home/icse-2025" target="_BLANK"><u>47th International Conference on Software Engineering (ICSE 2025)</u></a> in Ottawa, Canada.</p>
+
+<!--<p>SESoS information is also available at <a href="https://conf.researchr.org/home/icse-2022/sesos-2022" target="_blank"><u>https://conf.researchr.org/home/icse-2022/sesos-2022</u></a>.</p>-->
+
+					</div><br>
+<!-- <div class="fb-page fb_iframe_widget" data-href="https://www.facebook.com/sesos2025" data-width="500" data-hide-cover="false" data-show-facepile="true" data-show-posts="false" fb-xfbml-state="rendered" fb-iframe-plugin-query="app_id=&amp;container_width=663&amp;hide_cover=false&amp;href=https://www.facebook.com/sesos2025&amp;locale=en_US&amp;sdk=joey&amp;show_facepile=true&amp;show_posts=false&amp;width=500"><span style="vertical-align: bottom; width: 500px; height: 214px;"><iframe name="fe7d0c66c843b" width="500px" height="1000px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" title="fb:page Facebook Social Plugin" src="https://www.facebook.com/v2.3/plugins/page.php?app_id=&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter%2Fr%2F7LloFuHvA7I.js%3Fversion%3D43%23cb%3Df29bb0c871c2b6%26domain%3Dwdes2018.icmc.usp.br%26origin%3Dhttp%253A%252F%252Fwdes2018.icmc.usp.br%252Ff10390643fa624c%26relation%3Dparent.parent&amp;container_width=663&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2FWDES-Community-233717624032818%2F&amp;locale=en_US&amp;sdk=joey&amp;show_facepile=true&amp;show_posts=false&amp;width=500" style="border: none; visibility: visible; width: 500px; height: 214px;" class=""></iframe></span></div> -->
+
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fsesos2025&tabs&width=500&height=130&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId" width="500" height="130" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+<!-- <footer class="bllf">2025 &nbsp;&nbsp;<span>Ottawa, Canada</span></footer> -->
+		</div>
+		
+			</section>	
+			
+		</article>
+	
+</div>
+
+
+</body>
+</html>
+
