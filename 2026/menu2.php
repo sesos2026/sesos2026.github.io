13cc562ee6f67709ee2e02383f332d18e42288ae
Everton Cavalcante <evertonranielly@gmail.com>
2026-08-19 08:51:20 -0300

    Changing from PHP to HTML in the 2026 website

---
+++ 2026/menu2.php
@@ -0,0 +1,57 @@
+<?php $resp = "Coming soon . . ."; ?>
+<?php include ("imagem.php"); ?>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.2';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<script async="" src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+<div id="menu">
+		<aside>
+        <nav>
+            <ul id="menu">
+                <li><a href="index.php"> &nbsp HOME</a></li>
+                <li><a href="Topics.php"> &nbsp TOPICS OF INTEREST</a></li>
+                <li><a href="Dates.php"> &nbsp IMPORTANT DATES</a></li>
+                <li><a href="Submission.php"> &nbsp PAPER SUBMISSION AND REVIEW</a></li>
+                <li><a href="Papers.php"> &nbsp; ACCEPTED PAPERS &nbsp;&nbsp;<font size="10" color="red"><b><i>**NEW**</i></b></font></a></li>
+                <li><a href="Keynote.php"> &nbsp; KEYNOTE SPEAKER &nbsp;&nbsp;<font size="10" color="red"><b><i>**NEW**</i></b></font></a></li>
+                <li><a href="Workshop.php"> &nbsp; WORKSHOP PROGRAM &nbsp;&nbsp;<font size="10" color="red"><b><i>**NEW**</i></b></font></a></li>
+                <li><a href="Committee.php"> &nbsp PROGRAM COMMITTEE</a></li>
+                <li><a href="Organizers.php"> &nbsp WORKSHOP ORGANIZERS</a></li>
+                <li><a href="http://www.sesos-wdes.icmc.usp.br/" target="_blank">  &nbsp WORKSHOP SERIES</a></li>
+            </ul>
+
+            <div style="display: flex; justify-content: center; gap: 15px; margin-top: 20px;">
+              <!-- LinkedIn -->
+              <a href="https://www.linkedin.com/company/sesoscommunity/" target="_blank" style="background-color: #0077b5; color: white; width: 40px; height: 20px; display: flex; align-items: center; justify-content: center; border-radius: 20%;">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+
+              <!-- Instagram -->
+              <a href="https://www.instagram.com/sesoscommunity" target="_blank" style="background-color: #ac2bac; color: white; width: 40px; height: 20px; display: flex; align-items: center; justify-content: center; border-radius: 20%;">
+                <i class="fab fa-instagram"></i>
+              </a>
+
+              <!-- Twitter / X -->
+              <a href="https://x.com/sesoscommunity" target="_blank" style="background-color: #55acee; color: white; width: 40px; height: 20px; display: flex; align-items: center; justify-content: center; border-radius: 20%;">
+                <i class="fab fa-twitter"></i>
+              </a>
+              
+              <!-- Facebook -->
+              <a href="https://www.facebook.com/sesoscommunity" target="_blank" style="background-color: #3b5998; color: white; width: 40px; height: 20px; display: flex; align-items: center; justify-content: center; border-radius: 20%;">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </div>
+
+      </nav>
+  </aside>
+</div>
+
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+/>
